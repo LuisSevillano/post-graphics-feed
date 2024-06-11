f5e11436761ddbc7dd8b65f1e49ba18f64af6898
LuisSevillano <luissevillanopires@gmail.com>
2024-06-12 00:28:09 +0200

    Updates

--- app/immutable/chunks/config.CloTv5Zb.js
+++
@@ -1,1 +0,0 @@
-const s="Washington Post Graphics feed",t="A curated list of recent articles published by The Washington Post's graphics team.",e="https://luissevillano.net/post-graphics-feed/index.html",i="https://gist.githubusercontent.com/LuisSevillano/5535e65d355aec8dc9315005a4342f68/raw/460b98ff0824fea75f3701c3d0b99b71ea345295/cover.jpg";export{t as a,e as b,i as c,s};
